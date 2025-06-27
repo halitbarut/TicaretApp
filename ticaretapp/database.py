aaeaf50684b6c49e023d45ff7bf38527eb14d000
Mehmet Halit Barut <mhbarut66@gmail.com>
2025-06-27 10:43:10 +0300

    Fix database URL assignment and update requirements for pydantic-settings

--- ticaretapp/database.py
+++ ticaretapp/database.py
@@ -4,9 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from ticaretapp.config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.database_url
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.database_url, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
