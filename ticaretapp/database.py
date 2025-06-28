b5b2182464cb3de1c1678a938f8b597e899c6bf7
Mehmet Halit Barut <97824169+halitbarut@users.noreply.github.com>
2025-06-28 11:20:49 +0300

    Update database.py

--- ticaretapp/database.py
+++ ticaretapp/database.py
@@ -9,8 +9,12 @@ if settings.database_url.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    settings.database_url, connect_args=connect_args
+    settings.database_url, 
+    connect_args=connect_args,
+    pool_recycle=300,
+    pool_pre_ping=True
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
